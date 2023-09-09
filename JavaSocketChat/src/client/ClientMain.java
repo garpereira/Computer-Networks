e8af2f960d1a27b16630ec364d0d3b2790b80926
garpereira <garpereira@unifesp.br>
2023-09-09 00:27:33 -0300

    Reproducing ECHO from Server

--- JavaSocketChat/src/client/ClientMain.java
+++ JavaSocketChat/src/client/ClientMain.java
@@ -1,5 +1,21 @@
 package client;
+import java.net.Socket;
+import java.io.PrintStream;
 
 public class ClientMain {
-    
+    public static void main(String[] args){
+        // Dados do servidor | Para onde vão as informações
+        try{
+            Socket cliente = new Socket("127.0.0.1", 7000);
+            
+            System.out.println("Conectado ao servidor!");
+            // Fluxo de dados para envio
+            PrintStream output = new PrintStream(cliente.getOutputStream());
+            output.println("Olá, servidor!");
+            cliente.close();
+            System.out.println("Conexão encerrada!");
+        }catch(Exception e){
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
 }
