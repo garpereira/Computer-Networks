8cc27c0c04453bbb669db477cfac975e7cf75688
Matheus Silva <souza.matheus@unifesp.br>
2023-09-11 20:32:24 -0300

    Add threads

--- JavaSocketChat/src/client/ClientMain.java
+++ JavaSocketChat/src/client/ClientMain.java
@@ -1,4 +1,5 @@
 package client;
+
 import java.net.Socket;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -8,7 +9,7 @@ public class ClientMain {
     public static void main(String[] args) {
         try {
             Socket cliente = new Socket("127.0.0.1", 7000);
-            
+
             System.out.println("Conectado ao servidor! Digite 'sair' para encerrar a conexão.");
             ObjectOutputStream output = new ObjectOutputStream(cliente.getOutputStream());
             Scanner inputCliente = new Scanner(System.in);
@@ -16,16 +17,15 @@ public class ClientMain {
             // Recebendo a mensagem do servidor
             ObjectInputStream inputServer = new ObjectInputStream(cliente.getInputStream());
 
+            // Cria um novo thread para receber mensagens do servidor
+            Thread t = new Thread(new ServerHandler(inputServer));
+            t.start();
+
             while (true) {
                 System.out.println("Digite uma mensagem: ");
                 String mensagem = inputCliente.nextLine();
                 output.writeObject(mensagem);
 
-                String mensagemServer = inputServer.readObject().toString();
-                if (mensagemServer != null) {
-                    System.out.println("Mensagem recebida do Servidor: " + mensagemServer);
-                }
-                
                 if (mensagem.equals("sair")) {
                     break;
                 }
@@ -35,6 +35,27 @@ public class ClientMain {
             output.close();
             cliente.close();
             System.out.println("Conexão encerrada!");
+        } catch (Exception e) {
+            e.printStackTrace(); // Imprime a pilha de rastreamento da exceção
+        }
+    }
+}
+
+class ServerHandler implements Runnable {
+    private ObjectInputStream inputServer;
+
+    public ServerHandler(ObjectInputStream inputServer) {
+        this.inputServer = inputServer;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                String mensagemServer = inputServer.readObject().toString();
+                if (mensagemServer != null) {
+                    System.out.println("Mensagem recebida do Servidor: " + mensagemServer);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace(); // Imprime a pilha de rastreamento da exceção
         }
